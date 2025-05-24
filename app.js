1ef3180344dac942939a9f19e0b9ccf919aab5f6
Nikhil Sharma <nikhilsharma0437@gmail.com>
2025-05-24 22:50:46 +0530

    updated code

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 3000;
 require('dotenv').config();
-const schoolRoutes = require('./routes/school.js');
+const schoolRoutes = require('./routes/school');
 
-app.use('/', schoolRoutes);
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
-
+app.use('/', schoolRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
