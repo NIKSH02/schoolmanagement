3735220fe6d6c27f915e1fd3e9b0ebca9d3152af
Nikhil Sharma <nikhilsharma0437@gmail.com>
2025-05-25 02:14:12 +0530

    some more updation

--- schema.sql
+++ schema.sql
@@ -1,9 +1,7 @@
-
-
-CREATE TABLE user(
+CREATE TABLE schools(
     id INT AUTO_INCREMENT PRIMARY KEY ,
     name VARCHAR(50) UNIQUE NOT NULL ,
     address VARCHAR(50) NOT NULL,
     latitude FLOAT  NOT NULL,
     longitude FLOAT NOT NULL
-)
+);
